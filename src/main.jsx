9a6f15fe4325a287ae0c53e1c25ee6a5a6034127
Akash s <akashsivakumarjayalekshmi@gmail.com>
2025-10-16 15:20:34 +0530

    Implementing hero section

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import NavBar from './components/NavBar.jsx'
+import Hero from './components/Hero.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <NavBar />
+    <Hero/>
   </StrictMode>,
 )
